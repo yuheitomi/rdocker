a73ed98aaf7dd41a7bcd5c5bcd8a474944c1f31c
Yuhei Kuratomi <yuhei.kuratomi@gmail.com>
2024-05-02 18:48:42 +0900

    Update run.sh script to refer its directory name as a container name

--- rocker-rstudio-jp/run.sh
+++ rocker-rstudio-jp/run.sh
@@ -1,22 +1,26 @@
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+CONTAINER_NAME="$(basename $SCRIPT_DIR)"
+
 if [ ! -d workspace ]; then
-  mkdir workspace
+    mkdir workspace
 fi
 
-docker run -d \
-  --rm \
-  --name my-rstudio-jp \
-  -p 8787:8787 \
-  -e LC_ALL=en_US.UTF-8 \
-  -e TZ=Asia/Tokyo \
-  -e ROOT=true \
-  -e PASSWORD=password \
-  -e DISABLE_AUTH=true \
-  -e RENV_CONFIG_PAK_ENABLED=TRUE \
-  -e RENV_PATHS_ROOT=/home/rstudio/.cache/R/renv \
-  --mount type=bind,source=./workspace,target=/home/rstudio/workspace \
-  --mount type=bind,source=$HOME/.cache/R/renv,target=/home/rstudio/.cache/R/renv \
-  --mount type=bind,source=$HOME/.cache/R/pkgcache,target=/home/rstudio/.cache/R/pkgcache \
-  --mount type=bind,source=$HOME/.cache/rstudio-server,target=/home/rstudio/.cache/rstudio \
-  --mount type=bind,source=$HOME/.config/rstudio-server,target=/home/rstudio/.config/rstudio \
-  --mount type=bind,source=$HOME/.config/github-copilot,target=/home/rstudio/.config/github-copilot \
-  yuheitomi/rocker-rstudio-jp:latest
+docker run \
+    -d \
+    --rm \
+    --name $CONTAINER_NAME \
+    -p 8787:8787 \
+    -e LC_ALL=en_US.UTF-8 \
+    -e TZ=Asia/Tokyo \
+    -e ROOT=true \
+    -e PASSWORD=password \
+    -e DISABLE_AUTH=true \
+    -e RENV_CONFIG_PAK_ENABLED=TRUE \
+    -e RENV_PATHS_ROOT=/home/rstudio/.cache/R/renv \
+    --mount type=bind,source=./workspace,target=/home/rstudio/workspace \
+    --mount type=bind,source=$HOME/.cache/R/renv,target=/home/rstudio/.cache/R/renv \
+    --mount type=bind,source=$HOME/.cache/R/pkgcache,target=/home/rstudio/.cache/R/pkgcache \
+    --mount type=bind,source=$HOME/.cache/rstudio-server,target=/home/rstudio/.cache/rstudio \
+    --mount type=bind,source=$HOME/.config/rstudio-server,target=/home/rstudio/.config/rstudio \
+    --mount type=bind,source=$HOME/.config/github-copilot,target=/home/rstudio/.config/github-copilot \
+    yuheitomi/rocker-rstudio-jp:latest
