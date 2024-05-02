79cd4e82652b69cce86abf78a9262a06db9c101d
Yuhei Kuratomi <yuhei.kuratomi@gmail.com>
2024-05-02 23:15:18 +0900

    Update build.sh script to use docker buildx for multi-platform support

--- rocker-rstudio-jp/build.sh
+++ rocker-rstudio-jp/build.sh
@@ -1,2 +1,7 @@
-docker pull rocker/rstudio:4.4
-docker build . -t yuheitomi/rocker-rstudio-jp:latest
+# docker pull rocker/rstudio:4.4
+# docker build . -t yuheitomi/rocker-rstudio-jp:latest
+
+docker buildx build -t yuheitomi/rocker-rstudio-jp:latest . \
+    --platform linux/amd64,linux/arm64/v8 \
+    --output=type=image,push=true
+    
